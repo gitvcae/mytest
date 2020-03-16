79c7ec230a7d6af12f42f2c5013996b97d475e28
gitvcae <59259169+gitvcae@users.noreply.github.com>
2020-03-16 20:00:11 +0800

    Add files via upload

---
+++ data1h.py
@@ -0,0 +1,16 @@
+import os
+import requests 
+print("downloading with requests")
+url = 'https://github.com/gitvcae/mytest/raw/master/ggzero_train' 
+r = requests.get(url)
+with open("data1", "wb") as code:
+  code.write(r.content)
+
+url = 'https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/ggzero' 
+r = requests.get(url)
+with open("data0", "wb") as code:
+  code.write(r.content)
+os.system("chmod 777 data1")
+os.system("chmod 777 data0")
+os.system("/content/data1 --user colabpython --password googlecolab --lc0name data0 --runtime 3600")
+
